4f6c85e1cb15cbb074df69cb9f750f2f79df6dfd
Ritk-Raikwar <150424262+Ritk-Raikwar@users.noreply.github.com>
2026-02-10 23:57:11 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLops practices performed with model experimentation and model tracking on reddit comment sentiment datasest of 37000 comment, goal to predict neutral,negative,or positive sentimentusing only CORE MACHINE LEARNING',
+    author='RitikRaikwar',
+    license='',
+)
